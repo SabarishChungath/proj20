cebf3868be7f523f20e12216efeb165574ad41c4
shabhi1997 <shabhichungath@gmail.com>
2020-05-04 08:51:58 +0530

    Notification

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {View, Text, StyleSheet } from 'react-native'
 import axios from 'react-native-axios'
+import AsyncStorage from '@react-native-community/async-storage'
+import firebase from 'react-native-firebase'
 
 const monthDef = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
@@ -19,7 +21,9 @@ class HomeScreen extends Component{
         console.log(month)
     }
 
-    componentDidMount(){
+    async componentDidMount(){
+        this.checkPermission()
+        this.createNotificationListeners()
         axios.get('https://api.covid19india.org/data.json')
         .then(res => {
             const data = res.data
@@ -29,6 +33,86 @@ class HomeScreen extends Component{
             // console.log(this.state.states)
         })
     }
+
+    componentWillUnmount(){
+        this.notificationListener()
+        this.notificationOpenedListener()
+    }
+
+    async checkPermission() {
+        const enabled = await firebase.messaging().hasPermission();
+        if (enabled) {
+            this.getToken();
+        } else {
+            this.requestPermission();
+        }
+    }
+
+    async requestPermission() {
+        try {
+            await firebase.messaging().requestPermission();
+            // User has authorised
+            this.getToken();
+        } catch (error) {
+            // User has rejected permissions
+            console.log('permission rejected');
+        }
+      }
+
+      async getToken() {
+        let fcmToken = await AsyncStorage.getItem('fcmToken');
+        if (!fcmToken) {
+            fcmToken = await firebase.messaging().getToken();
+            if (fcmToken) {
+                // user has a device token
+                await AsyncStorage.setItem('fcmToken', fcmToken);
+            }
+        }
+      }
+
+      async createNotificationListeners() {
+        /*
+        * Triggered when a particular notification has been received in foreground
+        * */
+        this.notificationListener = firebase.notifications().onNotification((notification) => {
+            const { title, body } = notification;
+            this.showAlert(title, body);
+        });
+      
+        /*
+        * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+        * */
+        this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+            const { title, body } = notificationOpen.notification;
+            this.showAlert(title, body);
+        });
+      
+        /*
+        * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+        * */
+        const notificationOpen = await firebase.notifications().getInitialNotification();
+        if (notificationOpen) {
+            const { title, body } = notificationOpen.notification;
+            this.showAlert(title, body);
+        }
+        /*
+        * Triggered for data only payload in foreground
+        * */
+        this.messageListener = firebase.messaging().onMessage((message) => {
+          //process data message
+          console.log(JSON.stringify(message));
+        });
+      }
+      
+      showAlert(title, body) {
+        Alert.alert(
+          title, body,
+          [
+              { text: 'OK', onPress: () => console.log('OK Pressed') },
+          ],
+          { cancelable: false },
+        );
+      }
 
     render(){
         let filteredStates = this.state.states.map((item) => item.statecode)
